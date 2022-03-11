4d7c1d0899ce12e654af933f22de865ac081b91b
Ilya <ilya.skobelev.04@mail.ru>
2022-03-11 16:03:32 +0300

    stage 0.1

--- js/script.js
+++ js/script.js
@@ -24,3 +24,38 @@ const movieDB = {
     ]
 };
 
+const adverb = document.querySelectorAll('.promo__adv img'),
+    header = document.querySelector('.promo__genre'),
+    bg = document.querySelector('.promo__bg'),
+    list = document.querySelector('.promo__interactive-list'),
+    items = document.querySelectorAll('.promo__interactive-item');
+
+adverb.forEach(item => {
+    item.remove();
+});
+
+header.innerHTML = "драма";
+
+bg.style.background = "url(img/bg.jpg) center center/cover no-repeat";
+
+items.forEach(item => {
+    item.remove();
+});
+
+// или 
+// list.innerHTML = "";
+
+movieDB.movies.sort();
+movieDB.movies.forEach((item, index) => {
+    const div = document.createElement('li');
+    div.classList.add('promo__interactive-item');
+    div.innerHTML = `${index + 1} ${item} <div class="delete"></div>`;
+    list.append(div);
+    // или
+    // list.innerHTML += `
+    //     <li class="promo__interactive-item">${index + 1} ${item}
+    //         <div class="delete"></div>
+    //     </li>
+    // `;
+});
+
